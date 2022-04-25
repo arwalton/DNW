c16b0bdb3c2a591bad2c81dc4c8d27833b1032fb
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-04-25 12:43:00 +0300

    Add about page route to myFirstExpress

--- topic2/myFirstExpress/index.js
+++ topic2/myFirstExpress/index.js
@@ -4,3 +4,5 @@ const port = 8083;
 
 app.get("/",(req,res) => res.send("<h1>Hello World!</h1>"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/about",(req, res) => res.send("<h1>This is the about page</h1>"));
