493efe01fecdc31d95b79eea7c0a6622cc299b67
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-04-25 12:47:13 +0300

    Add search route to server

--- topic2/myFirstExpress/index.js
+++ topic2/myFirstExpress/index.js
@@ -4,6 +4,6 @@ const port = 8083;
 
 app.get("/",(req,res) => res.send("<h1>This is my home page</h1>"));
 app.get("/about",(req, res) => res.send("<h1>This is the about page</h1>"));
-
+app.get("/search",(req, res) => res.send("<h1>This will be a search page</h1>"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
