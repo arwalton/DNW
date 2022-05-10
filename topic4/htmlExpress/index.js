65b3d39f85b274545a08fc3eae8f7a7185d60e0c
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-05-10 10:01:24 +0300

    Add topic 4, basic server with routes and a small form.

---
+++ topic4/htmlExpress/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 8086;
+
+require("./routes/main")(app);
+
+app.set("views",__dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+app.listen(port, ()=>console.log(`Example app listening on port ${port}!`));
