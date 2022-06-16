a5fbd51448c4c7e466e7388fff8dc389dc027779
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-06-16 14:47:53 +0300

    Add about page and nodemon to project

--- topic7/mid-term/routes/main.js
+++ topic7/mid-term/routes/main.js
@@ -1,5 +1,11 @@
 module.exports = (app) => {
+    // Home route
     app.get("/", function(req,res){
         res.render("index.ejs")
+    });
+
+    // About page route
+    app.get("/about", function(req,res){
+        res.render("about.ejs")
     });
 }
