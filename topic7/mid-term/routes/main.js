2ef9d63dfac4f5cc196ae5c9947b7e1574afbacc
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-06-22 16:05:29 +0300

    Add additional SQL queries on new device page load

--- topic7/mid-term/routes/main.js
+++ topic7/mid-term/routes/main.js
@@ -12,29 +12,44 @@ module.exports = (app) => {
     // Add device route
     app.get("/new-device", (req,res)=>{
         // Query for existing devices
-        let sqlQuery1 = "SELECT * FROM devices;";
-        sqlQuery1 += "SELECT * FROM types;";
+        let sqlQuery = "SELECT * FROM devices;";
+        // Query for existing types
+        sqlQuery += "SELECT * FROM types;";
+        // Query for existing functions
+        sqlQuery += "SELECT * FROM functions;"
+        // Query to match types to available functions
+        sqlQuery += `SELECT types.typeName, functions.functionName
+                    FROM ((types
+                    INNER JOIN typeToFunctionMap
+                    ON types.typeId = typeToFunctionMap.typeId)
+                    INNER JOIN functions
+                    ON typeToFunctionMap.functionId = functions.functionId);`
         // Execute query
-        db.query(sqlQuery1, (err,result) => {
+        db.query(sqlQuery, (err,result) => {
             if(err){
                 res.redirect("/");
             }
-            // Extract device data from result
-            const devices = result[0].map(item =>{
+            // Convert data to useable format
+            const data = result.map(item =>{
                 let myStr = JSON.stringify(item);
                 let json = JSON.parse(myStr);
                 return json;
             })
-            // Extract type data from result
-            const types = result[1].map(item =>{
-                let myStr = JSON.stringify(item);
-                let json = JSON.parse(myStr);
-                return json;
-            })
+            //Create map for types and functions
+            const typeToFunction = new Map();
+            data[3].forEach((item) => {
+                if(typeToFunction.has(item.typeName)){
+                typeToFunction.set(item.typeName, [...typeToFunction.get(item.typeName), item.functionName]);
+                }else{
+                    typeToFunction.set(item.typeName,[item.functionName]);
+                }
+            });
             // Render page passing data as props
             res.render("new-device.ejs", {
-                devices: devices,
-                types: types
+                devices: data[0],
+                types: data[1],
+                functions: data[2],
+                typeToFunction: typeToFunction
              });
         })
     });
