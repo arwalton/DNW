3124fa8c9643a721c12c91904036bfe3fc9957ba
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-06-29 16:00:32 +0300

    Update sql tables and queries to work properly with add device page

--- topic7/mid-term/routes/main.js
+++ topic7/mid-term/routes/main.js
@@ -1,14 +1,17 @@
 module.exports = (app) => {
+    ////////////////////////////////////////
     // Home route
     app.get("/", (req,res)=>{
         res.render("index.ejs")
     });
 
+    ////////////////////////////////////////
     // About page route
     app.get("/about", (req,res)=>{
         res.render("about.ejs")
     });
 
+    ////////////////////////////////////////
     // Add device route
     app.get("/new-device", (req,res)=>{
         // Query for existing devices
@@ -57,13 +60,48 @@ module.exports = (app) => {
         })
     });
 
-    //Success page when device has been successfully added
+    ///////////////////////////////////////////
+    //Success page when device has been successfully added route
     app.post("/device-added", (req,res)=>{
         console.log("Here is where we will save the data in the database");
-        
         console.log(req.body);
-        res.render("device-added.ejs",{
-
+        const functions = [];
+        if(!req.body['on_off']){
+            functions.push('on_off', 0);
+        }else{
+            functions.push('on_off', 1);
+        }
+        Object.keys(req.body).forEach(key => {
+            if(key != 'device_name' && key != 'device_type' && key != 'on_off'){
+                if(req.body[key] != ''){
+                    functions.push(key, req.body[key]);
+                }
+            }
         });
+
+        let sqlQuery = "INSERT INTO devices (deviceName, typeId)" +
+                        "VALUES (\""+ req.body.device_name +
+                        "\",(SELECT typeId FROM types WHERE typeName=\"" + req.body.device_type +"\"));"
+        
+        for(let i=0; i<functions.length; i+=2){
+            sqlQuery += "INSERT INTO deviceToFunctionMap(deviceId, functionId, value) " +
+                        "VALUES ((SELECT (deviceId) FROM devices WHERE deviceName = \'" + req.body.device_name + "\')," +
+                        "(SELECT (functionId) FROM functions WHERE functionName = \'" + functions[i] + "\')," +
+                        functions[i + 1]+ ");"
+        }
+        console.log(sqlQuery);
+
+        try{
+        // Execute query
+        db.query(sqlQuery,(err, result)=>{
+            if(err){
+                res.redirect(404, "/");
+            }else{
+            res.render("device-added.ejs");
+            }
+        })
+        }catch(e){
+            console.log("Error: " + e);
+        } 
     })
 }
