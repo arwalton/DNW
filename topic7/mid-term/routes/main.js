61840d248c54c4b92a9f3f03fc8e231bd6224b22
Adam Walton <adamrwalton@gmail.comgit config user.name Adam>
2022-07-06 11:33:27 +0300

    Add delete device page and routes

--- topic7/mid-term/routes/main.js
+++ topic7/mid-term/routes/main.js
@@ -49,7 +49,6 @@ module.exports = (app) => {
                     typeToFunctionMap.set(item.typeName,[funcName]);
                 }
             });
-            const typeToFunction = Object.fromEntries(typeToFunctionMap);
             // Render page passing data as props
             res.render("new-device.ejs", {
                 devices: data[0],
@@ -152,9 +151,155 @@ module.exports = (app) => {
                 let json = JSON.parse(myStr);
                 return json;
             })
+            console.log(data)
             res.render("device-status.ejs",{
                 functions: data
             });
         });
+    })
+
+    ///////////////////////////////////
+    // Control device route
+    app.get("/control-device", (req,res)=>{
+        // Query for existing devices
+        const sqlQuery = `SELECT devices.deviceName, deviceToFunctionMap.value, functions.functionName
+                          FROM ((devices
+                          INNER JOIN deviceToFunctionMap
+                          ON devices.deviceId = deviceToFunctionMap.deviceId)
+                          INNER JOIN functions
+                          ON deviceToFunctionMap.functionId = functions.functionId);
+                          SELECT * FROM functions;`
+        // Execute query
+        db.query(sqlQuery, (err,result) => {
+            if(err){
+                res.redirect("/");
+            }
+            // Convert data to useable format
+            const data = result.map(item =>{
+                let myStr = JSON.stringify(item);
+                let json = JSON.parse(myStr);
+                return json;
+            })
+
+            // //Create a map for devices and functions
+            const deviceToFunctionMap = new Map();
+
+            data[0].forEach((device)=> {
+                if(deviceToFunctionMap.has(device.deviceName)){
+                    deviceToFunctionMap.set(device.deviceName, [...deviceToFunctionMap.get(device.deviceName),device.functionName]);
+                }else{
+                    deviceToFunctionMap.set(device.deviceName,[device.functionName]);
+                }
+            })
+
+            res.render("control-device.ejs", {
+                 devices: data[0],
+                // types: data[1],
+                 functions: data[1],
+                // typeToFunction: JSON.stringify(Object.fromEntries(typeToFunctionMap))
+                deviceToFunction: JSON.stringify(Object.fromEntries(deviceToFunctionMap))
+             });
+        })
+    });
+
+    ///////////////////////////////////////////
+    //Success page when device has been successfully updated
+    app.post("/device-updated", (req,res)=>{
+        // Create sql query
+        const functions = [];
+        if(!req.body['on_off']){
+            functions.push({functionName: 'on_off',
+                            value: '0'});
+        }else{
+            functions.push({functionName: 'on_off',
+                            value: '1'});
+        }
+        // Get device functions and values
+        Object.keys(req.body).forEach(key => {
+            if(key != 'device_name' && key != 'device_type' && key != 'on_off'){
+                if(req.body[key] != ''){
+                    functions.push({functionName: key, 
+                                    value: req.body[key]});
+                }
+            }
+        });
+        console.log(functions);
+
+        // Create SQL query to update each function of the device
+        let sqlQuery = "";
+
+        functions.forEach((func)=>{
+            sqlQuery += `UPDATE deviceToFunctionMap
+            SET value = ${func.value}
+            WHERE deviceId = (
+               SELECT deviceId 
+               FROM devices
+               WHERE deviceName="${req.body.device_name}")
+           AND functionId = (
+               SELECT functionId 
+               FROM functions
+               WHERE functionName="${func.functionName}");`
+        })
+
+        try{
+        // Execute query
+        db.query(sqlQuery,(err, result)=>{
+            if(err){
+                console.log("Error" + err)
+                res.redirect("/");
+            }else{
+                res.render("device-updated.ejs");
+            }
+        })
+        }catch(e){
+            console.log("Error: " + e);
+        } 
+    })
+    /////////////////////////////////////////////
+    // Delete device page
+    app.get("/delete-device", (req,res)=>{
+        let sqlQuery = "SELECT * FROM devices;"
+
+        try{
+            // Execute query
+            db.query(sqlQuery,(err, result)=>{
+                if(err){
+                    console.log("Error" + err)
+                    res.redirect("/");
+                }else{
+
+                // Convert data to useable format
+                const data = result.map(item =>{
+                    let myStr = JSON.stringify(item);
+                    let json = JSON.parse(myStr);
+                    return json;
+                })
+                    res.render("delete-device.ejs", {
+                        devices: data
+                    });
+                }
+            })
+        }catch(e){
+                console.log("Error: " + e);
+        } 
+    })
+
+    /////////////////////////////////////////////
+    app.post("/device-deleted", (req,res)=>{
+        let sqlQuery = `DELETE FROM devices WHERE deviceId=${req.body.devices};`
+
+        try{
+            // Execute query
+            db.query(sqlQuery,(err, result)=>{
+                if(err){
+                    console.log("Error" + err)
+                    res.redirect("/");
+                }else{
+                    res.render("device-deleted.ejs");
+                }
+            })
+        }catch(e){
+                console.log("Error: " + e);
+        } 
     })
 }
